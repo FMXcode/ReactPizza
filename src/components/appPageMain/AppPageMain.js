f64dbc060555f50a4eebe757eb3e881a4dabf0a9
fedor <fedor@email.com>
2022-12-21 19:24:44 +0300

    first commit

---
+++ src/components/appPageMain/AppPageMain.js
@@ -0,0 +1,12 @@
+import './appPageOne.scss';
+import AppHeader from "../appHeader/AppHeader";
+
+function AppPageOne(props) {
+    return (
+        <div className="appPageOne">
+            <AppHeader/>
+        </div>
+    );
+}
+
+export default AppPageOne;
